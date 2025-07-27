3779a55a64b128bd0bbd7abe7226febc6f2772ce
gtemate <dev.sekhmet@gmail.com>
2025-07-27 12:28:30 +0200

    Initial commit

---
+++ nyounda-infrastructure/nyounda-api/src/main/java/com/kezisoft/nyounda/api/NyoundaApi.java
@@ -0,0 +1,13 @@
+package com.kezisoft.nyounda.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyoundaApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NyoundaApi.class, args);
+    }
+
+}
